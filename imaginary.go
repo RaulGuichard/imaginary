4d8080d487e59c3562edf465063dc552fae723dc
Raul Guichard ProDesk <raul.guichard@gmail.com>
2018-10-03 07:25:21 -0500

    Max BigCache size.

--- imaginary.go
+++ imaginary.go
@@ -104,7 +104,31 @@ type URLSignature struct {
 	Key string
 }
 
-var cache, _ = bigcache.NewBigCache(bigcache.DefaultConfig(168 * time.Hour))
+var cache, _ = bigcache.NewBigCache(bigcache.Config{
+	// number of shards (must be a power of 2)
+	Shards: 1024,
+	// time after which entry can be evicted
+	LifeWindow: 60 * time.Minute,
+	// rps * lifeWindow, used only in initial memory allocation
+	MaxEntriesInWindow: 1000 * 10 * 60,
+	// max entry size in bytes, used only in initial memory allocation
+	MaxEntrySize: 500,
+	// prints information about additional memory allocation
+	Verbose: false,
+	// cache will not allocate more memory than this limit, value in MB
+	// if value is reached then the oldest entries can be overridden for the new ones
+	// 0 value means no size limit
+	HardMaxCacheSize: 1536,
+	// callback fired when the oldest entry is removed because of its expiration time or no space left
+	// for the new entry, or because delete was called. A bitmask representing the reason will be returned.
+	// Default value is nil which means no callback and it prevents from unwrapping the oldest entry.
+	OnRemove: nil,
+	// OnRemoveWithReason is a callback fired when the oldest entry is removed because of its expiration time or no space left
+	// for the new entry, or because delete was called. A constant representing the reason will be passed through.
+	// Default value is nil which means no callback and it prevents from unwrapping the oldest entry.
+	// Ignored if OnRemove is specified.
+	OnRemoveWithReason: nil,
+})
 
 func main() {
 
